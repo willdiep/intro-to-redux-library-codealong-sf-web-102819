11b1806677dd6c7e7e1dba6ea94fbf04600e790e
Will Diep <widiep@gmail.com>
2020-12-24 15:12:27 -0800

    commit tree 55e8a3dc52e23624c0cad7eae0d5f8899640c714

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 // ./src/index.js
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { createStore } from 'redux'; /* code change */
-import shoppingListItemReducer from './reducers/shoppingListItemReducer.js';
-import App from './App';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createStore } from 'redux' /* code change */
+import { Provider } from 'react-redux' /* code change */
+import shoppingListItemReducer from './reducers/shoppingListItemReducer.js'
+import App from './App'
+import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(
+  shoppingListItemReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
